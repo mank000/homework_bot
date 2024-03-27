0cbbb4cb500f1a367ca19bc8500929fee9aafe68
Козьмин Артём <kozmin_arti@mail.ru>
2024-03-27 21:27:47 +0700

    FirstCommit(2)

--- exceptions.py
+++ exceptions.py
@@ -1,19 +1,6 @@
-class EnvironmentVariableError(Exception):
-    """Исключение, связанное с переменными окружения."""
-    pass
-
-
 class RequestError(Exception):
     pass
 
 
-class ResponseError(Exception):
-    pass
-
-
 class ParseError(Exception):
-    pass
-
-
-class SendMessageError(Exception):
     pass
